4aef2b1efa2fed5847f1f38e416d1a4c6dc562a0
Chuck Conway <charles@cconway.com>
2016-02-24 22:12:28 -0800

    Added Redux

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Voting from './components/Voting';
+
+import {Provider} from 'react-redux';
+
+import {createStore} from 'redux';
+import reducer from './reducer';
+
 import {Router, Route} from 'react-router';
 import App from './components/App';
 import Results from './components/Results';
 
 const pair = ['Trainspotting', '28 Days Later'];
+
+const store = createStore(reducer);
+
+store.dispatch({
+	type:'SET_STATE',
+	state:{
+		vote:{
+			pair:['Sunshine', '28 Days Later'],
+			tally: {Sunshine:2}
+		}
+	}
+});
 
 const routes = <Route component={App}>
 								<Route path="/results" components={Results} />
@@ -13,6 +31,8 @@ const routes = <Route component={App}>
 							</Route>
 
 ReactDOM.render(
-	<Router>{routes}</Router>,
+	<Provider store={store}>
+		<Router>{routes}</Router>
+	</Provider>,
 	document.getElementById('app')
 );
